bb5d7e5d6e74ba66326f125c244e9893acaa06f0
Alicja Gałkiewicz <alicja.galkiewicz@exaco.pl>
2018-08-27 21:31:00 +0200

    #10 Add interceptor to globally handle errors

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,13 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {
   MatButtonModule,
   MatChipsModule, MatFormFieldModule, MatIcon, MatIconModule, MatInputModule, MatPaginatorIntl, MatPaginatorModule,
+  MatSnackBar, MatSnackBarModule,
   MatTableModule, MatToolbarModule
 } from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {RecipeService} from './recipe.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RecipesComponent} from './recipes/recipes.component';
 import {MessagesComponent} from './messages/messages.component';
 import {MessageService} from './message.service';
@@ -19,6 +20,8 @@ import {RecipeComponent} from './recipe/recipe.component';
 import {MatPaginatorIntlPL} from './mat-paginator-intl-pl';
 import {AppRoutingModule} from './app-routing.module';
 import {SignInComponent} from './sign-in/sign-in.component';
+import {Interceptor} from "./_interceptor/interceptor";
+import {Router} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -42,7 +45,8 @@ import {SignInComponent} from './sign-in/sign-in.component';
     MatInputModule,
     AppRoutingModule,
     MatChipsModule,
-    MatIconModule
+    MatIconModule,
+    MatSnackBarModule
   ],
   providers: [
     RecipeService,
@@ -50,6 +54,15 @@ import {SignInComponent} from './sign-in/sign-in.component';
     {
       provide: MatPaginatorIntl,
       useClass: MatPaginatorIntlPL
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Interceptor,
+      multi: true,
+      deps: [
+        Router,
+        MatSnackBar
+      ]
     }
   ],
   bootstrap: [AppComponent]
--- client/src/app/recipe.service.ts
+++ client/src/app/recipe.service.ts
@@ -1,49 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { catchError, tap } from 'rxjs/operators';
-import { RecipeUrlDto } from './dto/recipe-url-dto';
-import { Recipe } from './dto/recipe';
-import { MessageService } from './message.service';
-import { of } from 'rxjs/observable/of';
-
-const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {RecipeUrlDto} from './dto/recipe-url-dto';
+import {Recipe} from './dto/recipe';
 
 @Injectable()
 export class RecipeService {
 
   private recipesUrl = 'http://localhost:8080/recipes';
 
-  constructor(private http: HttpClient,
-              private messageService: MessageService) {
+  constructor(private http: HttpClient) {
   }
 
   add(url: RecipeUrlDto): Observable<Recipe> {
-    return this.http.post<Recipe>(this.recipesUrl, url, httpOptions)
-      .pipe(
-        tap((recipe: Recipe) => {
-          this.log(`Dodano przepis, id=${recipe.id}.`);
-          console.log(recipe);
-        }),
-        catchError(this.handleError<Recipe>('add recipe'))
-      );
+    return this.http.post<Recipe>(this.recipesUrl, url);
   }
 
   getAll(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.recipesUrl)
-      .pipe(
-        catchError(this.handleError('getAll', []))
-      );
+    return this.http.get<Recipe[]>(this.recipesUrl);
   }
 
   getOne(id: number): Observable<Recipe> {
     const url = `${this.recipesUrl}/${id}`;
-    return this.http.get<Recipe>(url).pipe(
-      tap(_ => this.log(`Wyciągnięto przepis, id=${id}`)),
-      catchError(this.handleError<Recipe>(`get recipe`, null))
-    );
+    return this.http.get<Recipe>(url);
   }
 
   searchByTerms(terms: string[]) {
@@ -51,40 +30,6 @@ export class RecipeService {
 
     const url = `${this.recipesUrl}/search?terms=${JSON.stringify(terms)}`;
 
-    return this.http.get<Recipe[]>(url).pipe(
-      tap(recipes => this.log(`Znaleziono przepisy: ${recipes}`)),
-      catchError(this.handleError('getAll', []))
-    );
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      if (error.status === 501) {
-        this.log(`Nie można przeanalizować treści podanej strony. Brak znaczników Schema.org`);
-      } else if (error.status === 409) {
-        this.log(`Przepis z podanej strony jest już zapisany.`);
-      } else {
-        this.log(`${operation} failed: ${error.message}`);
-      }
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  /** Log a RecipeService message with the MessageService */
-  private log(message: string) {
-    this.messageService.add('RecipeService: ' + message);
+    return this.http.get<Recipe[]>(url);
   }
 }
